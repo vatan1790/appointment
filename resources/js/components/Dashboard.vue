113394797f4fc4f3a244d58cbb6b83f03e456d78
workdeveloper2021 <work.developers2021@gmail.com>
2023-06-06 18:19:01 +0530

    dasbhoard

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1119,6 +1119,30 @@
               </div>
 
               <h4 class="heading-20-white mt-4">Working time</h4>
+              
+              <div id="exTab2" class="container">	
+              <ul class="nav nav-tabs">
+                    <li class="active">
+                      <a  href="#1" data-toggle="tab">Overview</a>
+                    </li>
+                    <li><a href="#2" data-toggle="tab">Without clearfix</a>
+                    </li>
+                    <li><a href="#3" data-toggle="tab">Solution</a>
+                    </li>
+                  </ul>
+
+                    <div class="tab-content ">
+                      <div class="tab-pane active" id="1">
+                        <h3>Standard tab panel created on bootstrap using nav-tabs</h3>
+                      </div>
+                      <div class="tab-pane" id="2">
+                        <h3>Notice the gap between the content and tab after applying a background color</h3>
+                      </div>
+                      <div class="tab-pane" id="3">
+                        <h3>add clearfix to tab-content (see the css)</h3>
+                      </div>
+                    </div>
+                </div>
               <div class="working-time mt-3">
                 <ul class="days text-center">
                   <li><button class="active">Mon</button></li>
@@ -2396,4 +2420,10 @@
         });
 
     });
+
+    function toggleTab(e){
+        var hrefVal = $(e).attr('href');
+        $('.nav-tabs li').removeClass('active');
+        $('.nav-tabs li[data-active="'+hrefVal+'"]').addClass('active');
+    }
  </script>       
