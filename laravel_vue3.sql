1c45237dd3da861d77465ac1a5a3de89ca788d7b
Senior Software Engineer <71081124+vatan1790@users.noreply.github.com>
2023-07-18 15:46:15 +0530

    Add files via upload

--- laravel_vue3.sql
+++ laravel_vue3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2023 at 04:01 PM
+-- Generation Time: Jul 18, 2023 at 12:10 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `laravel_vue3`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `technician_id` int(11) DEFAULT NULL,
+  `services` varchar(255) DEFAULT NULL,
+  `from_time` time DEFAULT NULL,
+  `to_time` time DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `customer_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `user_id`, `technician_id`, `services`, `from_time`, `to_time`, `created_at`, `updated_at`, `customer_id`) VALUES
+(1, 1, 1, '1,2', '12:00:00', '12:00:00', '2023-07-11 06:43:52', '2023-07-11 06:43:52', NULL),
+(2, 1, 2, '13', '01:00:00', '01:00:00', '2023-07-15 02:54:31', '2023-07-15 02:54:31', NULL),
+(3, 1, 2, '13', '01:00:00', '01:00:00', '2023-07-15 04:48:29', '2023-07-15 04:48:29', NULL);
 
 -- --------------------------------------------------------
 
@@ -42,11 +69,8 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `user_id`, `name`, `color`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 'rakesh', 'd', 1, '2023-04-22 03:08:53', '2023-04-22 03:08:53'),
-(2, 1, 'test', 'purple', 1, '2023-04-22 03:58:47', '2023-04-22 03:58:47'),
-(3, 1, 'test', 'purple', 1, '2023-04-22 03:59:19', '2023-04-22 03:59:19'),
-(4, 1, 'test', 'purple', 1, '2023-04-22 03:59:40', '2023-04-22 03:59:40'),
-(5, 1, 'test', 'purple', 1, '2023-04-25 03:18:02', '2023-04-25 03:18:02');
+(1, 1, 'Madisin', 'orange', 1, '2023-06-15 06:02:27', '2023-06-15 06:02:27'),
+(2, 1, 'Newcategory', 'purple', 1, '2023-06-15 06:29:51', '2023-06-15 06:29:51');
 
 -- --------------------------------------------------------
 
@@ -70,9 +94,9 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `user_id`, `fname`, `lname`, `phone`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 'rakesh', 'lname', '8989898989', 1, '2023-04-21 23:54:57', '2023-04-21 23:54:57'),
-(2, 1, 'dd', 'sdds', '555555', 1, '2023-04-22 01:23:34', '2023-04-22 01:23:34'),
-(3, 1, 'dd', 'sdds', '43434343', 1, '2023-04-22 01:27:38', '2023-04-22 01:27:38');
+(1, 1, 'Rakesh', 'Verma', '8989898989', 1, '2023-04-21 23:54:57', '2023-06-15 11:23:26'),
+(6, 1, 'Ravi', 'Kant', '5868585895', 1, '2023-06-15 06:00:51', '2023-06-15 06:00:51'),
+(7, 1, 'Shyam', 'Sharma', '9696958585', 1, '2023-06-15 06:28:29', '2023-06-15 06:28:29');
 
 -- --------------------------------------------------------
 
@@ -134,8 +158,8 @@ CREATE TABLE `packages` (
 --
 
 INSERT INTO `packages` (`id`, `user_id`, `name`, `price`, `image`, `status`, `created_at`, `updated_at`) VALUES
-(2, 1, 'dsdds', 100.00, NULL, 1, '2023-04-25 05:35:50', '2023-04-25 05:35:50'),
-(3, 1, 'dsdds', 55.00, '/storage/uploads/1682429554_cheesiano-pizza (1).png', 1, '2023-04-25 08:02:34', '2023-04-25 08:02:34');
+(1, 1, 'New Package', 4000.00, '/uploads/1686830556.jpg', 1, '2023-06-15 06:32:36', '2023-06-15 06:32:36'),
+(2, 1, 'New Package', 4000.00, '/uploads/1686830557.jpg', 1, '2023-06-15 06:32:37', '2023-06-15 06:32:37');
 
 -- --------------------------------------------------------
 
@@ -170,12 +194,35 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `id` int(11) NOT NULL,
+  `from_time` time DEFAULT NULL,
+  `to_time` time DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`id`, `from_time`, `to_time`, `created_at`, `updated_at`, `user_id`) VALUES
+(8, '01:00:00', '01:00:00', '2023-06-27 05:41:51', '2023-06-27 05:41:51', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `services`
 --
 
 CREATE TABLE `services` (
   `id` int(255) NOT NULL,
   `user_id` int(255) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   `duration` int(255) NOT NULL,
@@ -190,16 +237,59 @@ CREATE TABLE `services` (
 -- Dumping data for table `services`
 --
 
-INSERT INTO `services` (`id`, `user_id`, `name`, `image`, `duration`, `price`, `description`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 'test', '', 55, 100.00, 'sfsdfasdfdasf', 1, '2023-04-25 04:12:48', '2023-04-25 04:12:48'),
-(2, 1, 'sdds', '', 10, 100.00, 'dsds', 1, '2023-04-25 04:24:30', '2023-04-25 04:24:30'),
-(3, 1, 'sdds', '', 10, 100.00, 'cdds', 1, '2023-04-25 04:55:49', '2023-04-25 04:55:49'),
-(4, 1, 'sdds', '', 10, 100.00, 'sddsds', 1, '2023-04-25 06:16:45', '2023-04-25 06:16:45'),
-(5, 1, 'dsds', '', 10, 100.00, 'sdds', 1, '2023-04-25 07:06:53', '2023-04-25 07:06:53'),
-(6, 1, 'test', '/storage/uploads/1682426294_download (2).jpg', 55, 100.00, 'sfsdfasdfdasf', 1, '2023-04-25 07:08:14', '2023-04-25 07:08:14'),
-(7, 1, 'dsds', '', 10, 100.00, 'sdd', 1, '2023-04-25 07:08:55', '2023-04-25 07:08:55'),
-(8, 1, 'sdds', '/storage/uploads/1682428836_cheesiano-pizza (1).png', 10, 100.00, 'dsdssd', 1, '2023-04-25 07:50:36', '2023-04-25 07:50:36'),
-(9, 1, 'sdd', '/storage/uploads/1682430263_cheesiano-pizza (1).png', 10, 100.00, 'dsds', 1, '2023-04-25 08:14:23', '2023-04-25 08:14:23');
+INSERT INTO `services` (`id`, `user_id`, `category_id`, `name`, `image`, `duration`, `price`, `description`, `status`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 'Package', '/uploads/1688473319.webp', 23, 45.00, 'sdsdd', 1, '2023-07-04 06:51:59', '2023-07-08 06:24:09'),
+(2, 1, 2, 'Package 2', '/uploads/1688473395.webp', 30, 20.00, 'xzxxz', 1, '2023-07-04 06:53:15', '2023-07-08 06:24:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `technicians`
+--
+
+CREATE TABLE `technicians` (
+  `id` int(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `lname` varchar(255) NOT NULL,
+  `image` varchar(225) NOT NULL,
+  `services` varchar(255) DEFAULT NULL,
+  `schedules` varchar(255) DEFAULT NULL,
+  `workingTime` varchar(225) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `technicians`
+--
+
+INSERT INTO `technicians` (`id`, `user_id`, `fname`, `lname`, `image`, `services`, `schedules`, `workingTime`, `status`, `created_at`, `updated_at`) VALUES
+(2, 1, 'test new', 'Doe2', '/uploads/1688029002.jpg', '3,4', '8', 'mon|3:00:00,mon|11:00:00,mon|12:00:00', 1, '2023-06-29 03:26:42', '2023-06-29 03:26:42'),
+(3, 1, 'test new', 'dsds', '/uploads/1688029091.webp', '4,3', NULL, 'mon|11:00:00,mon|5:00:00,mon|6:00:00,mon|14:00:00', 1, '2023-06-29 03:28:11', '2023-06-29 03:28:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `temp_services`
+--
+
+CREATE TABLE `temp_services` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(11) NOT NULL,
+  `service_id` varchar(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `temp_services`
+--
+
+INSERT INTO `temp_services` (`id`, `user_id`, `service_id`, `created_at`, `updated_at`) VALUES
+(13, '1', '1', '2023-07-15 02:23:06', '2023-07-15 02:23:06'),
+(14, '1', '2', '2023-07-15 02:34:21', '2023-07-15 02:34:21');
 
 -- --------------------------------------------------------
 
@@ -228,6 +318,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `re
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `categories`
@@ -275,9 +371,27 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `services`
 --
 ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `technicians`
+--
+ALTER TABLE `technicians`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `temp_services`
+--
+ALTER TABLE `temp_services`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -292,16 +406,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -319,7 +439,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `packages`
 --
 ALTER TABLE `packages`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -328,10 +448,28 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `schedules`
+--
+ALTER TABLE `schedules`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `technicians`
+--
+ALTER TABLE `technicians`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `temp_services`
+--
+ALTER TABLE `temp_services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
